4d3a41ed2c994d3bb5e8298f0e54dd4fa6b3881e
AdivarekarBhumit <bhumit97ad@gmail.com>
2018-06-13 23:27:06 +0530

    Added Data and Utils.py file

---
+++ utils.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np 
+import glob
+import os 
+import pickle as pb     
+
+
+## We are using fasttext word embeddings
+
+## The embeddings are already stored in pickle file
