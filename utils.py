3c10280d6bab105a81e02c75ec9089ca482061f2
AdivarekarBhumit <bhumit97ad@gmail.com>
2018-06-14 10:01:36 +0530

    Add utils functions

--- utils.py
+++ utils.py
@@ -2,9 +2,112 @@ import pandas as pd
 import numpy as np 
 import glob
 import os 
-import pickle as pb     
+import pickle as pb    
+from nltk.corpus import stopwords 
+import re 
 
+
+stop_words = set(stopwords('english'))
+
+contractions = { 
+            "ain't": "am not",
+            "aren't": "are not",
+            "can't": "cannot",
+            "can't've": "cannot have",
+            "'cause": "because",
+            "could've": "could have",
+            "couldn't": "could not",
+            "couldn't've": "could not have",
+            "didn't": "did not",
+            "doesn't": "does not",
+            "don't": "do not",
+            "hadn't": "had not",
+            "hadn't've": "had not have",
+            "hasn't": "has not",
+            "haven't": "have not",
+            "he'd": "he would",
+            "he'd've": "he would have",
+            "he'll": "he will",
+            "he's": "he is",
+            "how'd": "how did",
+            "how'll": "how will",
+            "how's": "how is",
+            "i'd": "i would",
+            "i'll": "i will",
+            "i'm": "i am",
+            "i've": "i have",
+            "isn't": "is not",
+            "it'd": "it would",
+            "it'll": "it will",
+            "it's": "it is",
+            "let's": "let us",
+            "ma'am": "madam",
+            "mayn't": "may not",
+            "might've": "might have",
+            "mightn't": "might not",
+            "must've": "must have",
+            "mustn't": "must not",
+            "needn't": "need not",
+            "oughtn't": "ought not",
+            "shan't": "shall not",
+            "sha'n't": "shall not",
+            "she'd": "she would",
+            "she'll": "she will",
+            "she's": "she is",
+            "should've": "should have",
+            "shouldn't": "should not",
+            "that'd": "that would",
+            "that's": "that is",
+            "there'd": "there had",
+            "there's": "there is",
+            "they'd": "they would",
+            "they'll": "they will",
+            "they're": "they are",
+            "they've": "they have",
+            "wasn't": "was not",
+            "we'd": "we would",
+            "we'll": "we will",
+            "we're": "we are",
+            "we've": "we have",
+            "weren't": "were not",
+            "what'll": "what will",
+            "what're": "what are",
+            "what's": "what is",
+            "what've": "what have",
+            "where'd": "where did",
+            "where's": "where is",
+            "who'll": "who will",
+            "who's": "who is",
+            "won't": "will not",
+            "wouldn't": "would not",
+            "you'd": "you would",
+            "you'll": "you will",
+            "you're": "you are"
+            }
 
 ## We are using fasttext word embeddings
 
 ## The embeddings are already stored in pickle file
+def open_csv(path='./Data/yelp_dataset.txt'):
+    df = pd.read_csv(path, sep='\t')
+    return df 
+
+def clean_text(text, remove_stopwords=True):
+    text = text.lower()
+    text = text.split()
+
+    new_text = []
+
+    for word in text:
+        if word in contractions:
+            new_text.append(contractions[word])
+        else:
+            new_text.append(word)
+    
+    text = ' '.join(new_text)
+
+    text = re.sub(r'[_"\-;%()|+&=*%.,!?:#$@\[\]/]', ' ', text)
+    text = ' '.join(text.split())
+
+    return text
+
